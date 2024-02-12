a5463f7b95b4902c3626743029b48a5a21c4f719
chachacha12 <chaminwoo369@gmail.com>
2024-02-12 21:50:20 +0900

    FEAT: Get kaikas wallet addr

--- app/src/main/java/com/example/bicapplication/LoginActivity.kt
+++ app/src/main/java/com/example/bicapplication/LoginActivity.kt
@@ -3,8 +3,15 @@ package com.example.bicapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import com.example.bicapplication.databinding.ActivityLoginBinding
 import com.example.bicapplication.klaytn.Connect2KlaytnActivity
+import com.example.bicapplication.klaytn.ResultRespData
+import com.example.bicapplication.retrofit.RetrofitInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -16,9 +23,16 @@ class LoginActivity : AppCompatActivity() {
         //로그인 -> Connect2Klaytn으로 이동
         binding.apply {
             imagebtnKaikasLogin.setOnClickListener {
-                val intent = Intent(this@LoginActivity, MainActivity::class.java) ///MainActivity
+                val intent = Intent(this@LoginActivity, Connect2KlaytnActivity::class.java) ///MainActivity
                 startActivity(intent)  //Connect2KlaytnActivity
+                finish()
             }
         }
     }
+
+
+
+
+
+
 }
