c7a27c937d3134d6f1ee26c82fc56e55a2f33db6
chachacha12 <chaminwoo369@gmail.com>
2024-02-16 20:01:35 +0900

    FIX: Modify UserData field type

--- app/src/main/java/com/example/bicapplication/datamodel/UserData.kt
+++ app/src/main/java/com/example/bicapplication/datamodel/UserData.kt
@@ -6,10 +6,10 @@ data class UserData(
     var walletAddr : String?=null,
     var githubId : String?=null,
     var prizeMoney : Int = 0,  //총 얻은 상금값
-    var stack1 : List<ChallData>? =null,  //완료한 코딩 챌린지들
-    var stack2 : List<ChallData>? =null,  //완료한  시사교양  챌린지들
-    var stack3 : List<ChallData>? =null,  //완료한 신체단련 챌린지들
-    var stack4 : List<ChallData>? =null,  //완료한 생활 챌린지들
-    var progress_chall :List<ChallData>? =null //유저가 현재 진행중인 챌린지 목록
+    var stack1 : List<String>? =null,  //완료한 코딩 챌린지들
+    var stack2 : List<String>? =null,  //완료한  시사교양  챌린지들
+    var stack3 : List<String>? =null,  //완료한 신체단련 챌린지들
+    var stack4 : List<String>? =null,  //완료한 생활 챌린지들
+    var progress_chall :List<String>? =null //유저가 현재 진행중인 챌린지 목록
 ) {
 }
